3ab705fdbdb94f117e736721977c7d6f1dfc54b7
hmhornung <85147335+hmhornung@users.noreply.github.com>
2023-12-07 14:14:15 -0600

    finetuning

--- image_extractor.py
+++ image_extractor.py
@@ -1,9 +1,12 @@
 import cv2 as cv
+import pandas as pd
+import numpy as np
 
-
+#folder path constant
+folder_path = "train/game4/"
 
 #fill in with information
-video_path = "train/game4/input/game_4.mp4"
+video_name = "game_4.mp4"
 
 #destination folder path
 dest_path = "train/game4/images/"
@@ -14,19 +17,28 @@ file_extension = ".jpg"
 
 
 # Source the video feed
-cap = cv.VideoCapture(video_path)
+cap = cv.VideoCapture(folder_path + "input/"+  video_name)
 i = 0
+data = pd.read_csv(folder_path + "data/formatted_data.csv")
+
+arr = pd.read_csv(folder_path + "data/valid_idx.csv")
+valid_idx = arr.to_numpy()
 
 # While there is video, run the model per frame of the video
-while i < 10:
+while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+    
     # Resize optional
     # frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    cv.imwrite(dest_path + name_base + str(i) + file_extension, frame)
+    if(i in valid_idx): 
+        cv.imwrite(dest_path + name_base + str(i) + file_extension, frame)
+        print("valid " + str(i))
+    else:
+        print("invalid " + str(i))
     i += 1
 
+print("final i:" + str(i))
 # Release the video
 cap.release()
